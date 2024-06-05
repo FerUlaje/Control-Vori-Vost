507682d94c57bff0a9146832e0d89b57ccb77ea0
FerUlaje <ulajefernando@gmail.com>
2024-06-05 11:22:06 -0600

    Hrs Extra Pinwin

--- vv.py
+++ vv.py
@@ -17,6 +17,12 @@ data2 = {
     'dia': [15, 10, 15, 16, 12, 12, 13]
 }
 
+data3 = {
+    'semana': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
+    'hrs': [106, 224, 284, 369, 351, 268, 460, 385, 450, 397, 314, 229, 157, 199, 136, 184, 164, 139, 144, 129, 138, 227]
+}
+no_hrs = pd.DataFrame(data3)
+
 destajo_des = pd.DataFrame(data2)
 
 st.title('_Control_ :red[Vori Vost] :sunglasses:') # titulo de la página
@@ -28,3 +34,7 @@ if dest_button:
     st.dataframe(total_dest, hide_index=True)
 
 hrs_extra = st.button('Horas Extras') # creando botón para ver horas extras
+
+if hrs_extra:
+    st.line_chart(no_hrs, x='semana', y='hrs', color="#FF0000")
+    st.dataframe(no_hrs, hide_index=True)
